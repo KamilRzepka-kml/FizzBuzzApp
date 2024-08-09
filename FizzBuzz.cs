995e4114a8a3c83c449cf80bb46f8ca0301ae677
KML <rzepekvp93@gmail.com>
2024-08-09 16:42:27 +0200

    Add Project Files

---
+++ FizzBuzz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApp
+{
+    internal class FizzBuzz
+    {
+        public string GetNumber(int number)
+        {
+
+            if (number % 15 == 0)
+
+                return "FizzBUzz";
+
+            if (number % 3 == 0)
+
+                return "Fizz";
+
+            if (number % 5 == 0)
+
+                return "Buzz";
+
+            return number.ToString();
+
+        }
+
+        //public string GetNumber(int number)
+        //{
+        //    var result = string.Empty;
+        //    if (number % 15 == 0)
+        //    {
+        //        result = "FizzBUzz";
+        //    }
+
+        //    else if (number % 3 == 0)
+        //    {
+        //        result = "Fizz";
+        //    }
+
+        //    else if (number % 5 == 0)
+        //    {
+        //        result = "Buzz";
+        //    }
+
+        //    return result;
+
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            Console.WriteLine("Program FizzBuzz, który sprawdza czy liczba jest podzielna przez: \njeżeli przez 3, wtedy wypisze Fizz, jeżeli przez 5 to Buzz, a jeśli przez 15 to FizzBuzz");
+
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                var userNumber = NumberChecked();
+
+                Console.WriteLine(fizzBuzz.GetNumber(userNumber));
+                continue;
+            }
+
+        }
+
+        private static int NumberChecked()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out var number))
+                {
+                    Console.WriteLine("Podana wartość nie jest liczbą całkowitą. \nSpróbuj ponownie \nPodajliczbę: ");
+                    continue;
+                }
+
+                return number;
+
+            }
+
+        }
+    }
+}
